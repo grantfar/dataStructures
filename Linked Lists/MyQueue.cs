44a46e84496fe11b6a5db7a52633d1ba62d8add2
Grant Farnsworth <grantfar@gmail.com>
2015-10-27 15:43:12 -0400

    Added Queue and Stack

---
+++ Linked Lists/MyQueue.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linkedLists
+{
+    public class MyQueue<T>
+    {
+        private Node<T> head,end;
+        private int count;
+        public MyQueue()
+        {
+            count = 0;
+        }
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public void Add(T toAdd)
+        {
+            Node<T> tmpNode = new Node<T>(toAdd);
+            if (head == null)
+                head = tmpNode;
+            else
+                end.Next = tmpNode;
+            end = tmpNode;
+            count++;
+        }
+
+        public T get()
+        {
+            T returnT = head.Data;
+            head = head.Next;
+            count--;
+            return returnT;
+        }
+    }
+}
---
+++ Linked Lists/MyStack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linkedLists
+{
+    public class MyStack<T>
+    {
+        private Node<T> head;
+        private int count;
+        public MyStack()
+        {
+            count = 0;
+        }
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public void Add(T toAdd)
+        {
+            Node<T> tmpNode = new Node<T>(toAdd);
+            tmpNode.Next = head;
+            head = tmpNode;
+            count++;
+        }
+
+        public T get()
+        {
+            T returnT = head.Data;
+            head = head.Next;
+            count--;
+            return returnT;
+        }
+    }
+}
